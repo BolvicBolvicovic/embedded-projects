cf8055ccdfc1376163e2d9e2c5320766d0b9760c
Victor Cornille <victor.cornille@gmail.com>
2025-10-29 20:25:22 +0100

    wip: hc-sr04, currently blocking

---
+++ hc-sr04/hc-sr04.ino
@@ -0,0 +1,42 @@
+// Formula to calculate a distance based on ultra sonic waves:
+// D = 1/2 × T × C
+// where D is the distance, T is the time between the Emission and Reception, and C is the sonic speed.
+// (The value is multiplied by 1/2 because T is the time for go-and-return distance.)
+
+#define SERIAL_BAUDRATE	115200
+#define TRIGGER_PIN		D2
+#define ECHO_PIN		D3
+
+#define TC				0.034
+#define CALC_DIST(d)	(d * TC / 2)
+
+long	duration;
+int		distance;
+
+void
+setup()
+{
+	Serial.begin(SERIAL_BAUDRATE);
+	pinMode(TRIGGER_PIN, OUTPUT);
+	pinMode(ECHO_PIN, INPUT);
+	Serial.println("Setup DONE");
+}
+
+void
+loop()
+{
+	digitalWrite(TRIGGER_PIN, LOW);
+	delayMicroseconds(2);
+
+
+	digitalWrite(TRIGGER_PIN, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TRIGGER_PIN, LOW);
+
+	duration = pulseIn(ECHO_PIN, HIGH);
+	distance = CALC_DIST(duration);
+
+	Serial.print("Distance: ");
+	Serial.println(distance);
+	delay(500);
+}
